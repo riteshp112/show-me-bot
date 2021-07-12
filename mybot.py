ba77a4ae2e7aeb779638a9205f95aa34d8cee8e8
riteshp112 <50243841+riteshp112@users.noreply.github.com>
2021-07-12 19:15:14 +0530

    Add files via upload

---
+++ mybot.py
@@ -0,0 +1,30 @@
+from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
+from serpapi import GoogleSearch
+import os
+import requests
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+def start(Update,context):
+    Update.message.reply_text("What do you want to see")
+def text(Update,context):
+    query=Update.message.text
+    theurl="https://www.google.com/search?q="+query+"&source=lnms&tbm=isch"
+    r = requests.get(theurl)
+    #print(r)
+    soup = BeautifulSoup(r.text, "lxml")
+    #print(soup)
+    link = soup.find("img", {"class":"t0fcAb","src":True})['src']
+    #print(link)
+    Update.message.reply_photo(link)
+TOKEN="1619203815:AAFVMcnbJ_dk5W4a0XlEN6s1grx0pDrv8xI"
+updater=Updater(TOKEN,use_context=True)
+dispatcher=updater.dispatcher
+dispatcher.add_handler(CommandHandler("start",start))
+dispatcher.add_handler(MessageHandler(Filters.text,text))
+updater.start_polling()
+updater.idle()
+
+
+
+
+
