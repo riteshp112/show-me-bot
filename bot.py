b1289623a1dd67bac2ec04d5186e8f274c2e932d
Nitesh <riteshp112@gmail.com>
2021-08-01 17:11:26 +0530

    added files

--- bot.py
+++ bot.py
@@ -1,68 +1,30 @@
-import logging
-
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hey this is your bot!')
-
-
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Currently I am in Alpha stage, help me also!')
-
-def piracy(update, context):
-    update.message.reply_text('Ahhan, FBI wants to know your location!')
-
-
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
+from serpapi import GoogleSearch
+import os
+import requests
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+def start(Update,context):
+    Update.message.reply_text("What do you want to see")
+def text(Update,context):
+    query=Update.message.text
+    theurl="https://www.google.com/search?q="+query+"&source=lnms&tbm=isch"
+    r = requests.get(theurl)
+    #print(r)
+    soup = BeautifulSoup(r.text, "lxml")
+    #print(soup)
+    link = soup.find("img", {"class":"t0fcAb","src":True})['src']
+    #print(link)
+    Update.message.reply_photo(link)
+TOKEN="1619203815:AAFVMcnbJ_dk5W4a0XlEN6s1grx0pDrv8xI"
+updater=Updater(TOKEN,use_context=True)
+dispatcher=updater.dispatcher
+dispatcher.add_handler(CommandHandler("start",start))
+dispatcher.add_handler(MessageHandler(Filters.text,text))
+updater.start_polling()
+updater.idle()
 
 
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
-def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater("//TOKEN//", use_context=True)
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("piracy", piracy))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
